1d2b1a65def8d742c2eca607e5335bbd4946d9ac
MN.Kim <dymy2003@gmail.com>
2021-01-07 00:12:00 +0900

    While 1-1

---
+++ While1-1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;    // 시작
+            while (num < 20)// 종료조건
+            {
+                ++num;      // 변화
+                Console.WriteLine("나무를 {0:d2}번 찍었습니다", num);
+            }
+        }
+    }
+}
